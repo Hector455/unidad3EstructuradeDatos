2dde47894c5f5d1bf2f7896b0dcfb965929d18f1
Hector455 <america.12.tano@gmail.com>
2025-10-15 22:06:06 -0600

    U3A1
    aqui esta el proyecto de calificaciones grafico funcionando hasta agregar los 10 datos

---
+++ src/ejercicio1/datos/KardexDatos.java
@@ -0,0 +1,40 @@
+/*
+
+ */
+package ejercicio1.datos;
+
+/**
+ *Esta clase me permite mantener la informacion entre las ventanas
+ * 
+ * @author LENOVO Hector
+ * 14 de octubre
+ */
+public class KardexDatos {
+    public static String [][] datos=
+            new String [10][3];
+    public static int index=0; // para saber cuantos llevo
+    
+public static double calcularPromedio() {
+    if (KardexDatos.index == 0) return 0;
+
+    double suma = 0;
+    for (int i = 0; i < KardexDatos.index; i++) {
+        suma += Double.parseDouble(KardexDatos.datos[i][2]);
+    }
+    return suma / KardexDatos.index;
+    // metodo que calcula el promedio segun lo vallan ingresando
+}
+public static int  buscar(String dato){
+    int a=0;
+    for (int i=0;i<datos.length;i++){
+        if (dato.equals(datos[i][0])){
+             a=i;
+        }
+        else {
+            a=-1;           
+        }
+     
+    }
+    return a;
+} 
+}
